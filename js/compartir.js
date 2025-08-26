d1d593efeeb4330cd11947d5014884d5a86d8d84
IvanLuthje <luthjeivan@gmail.com>
2025-08-26 18:27:46 -0300

    Se corrige el regex de email y cambio de estilo en boton de volver

--- js/compartir.js
+++ js/compartir.js
@@ -16,26 +16,29 @@ function reset() {
 
 function enviar() {
     const form = document.getElementById('formulario_compartir');
-    const emailr = form.email_receptor.value;
+    var emailr = form.email_receptor.value;
     var alert_correo = `<i class='fas fa-exclamation-triangle'></i> Debe ingresar el correo electrónico`;
     var alert_redirect = `<i class="fa fa-external-link" aria-hidden="true"></i> Redireccionando al gestor de correo`;
+    var alert_valido = `<i class='fas fa-exclamation-triangle'></i> Debe ingresar el correo electrónico válido`;
+
+    var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 
     if (emailr == "") {
         $(".alert").html(alert_correo);
         return false;
     }
 
-    if (emailr == "/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/") {
-        $(".alert").html("Debe ingresar el correo electrónico válido");
+    if (!emailRegex.test(emailr)){
+        $(".alert").html(alert_valido);
         return false;
     }
 
-    else {
-        $(".alert").html(alert_redirect)
+  
+    $(".alert").html(alert_redirect)
         window.location = 'mailto: ' + $("#email_receptor").val() + '?subject=' + $("#subject").val() + '&body=' + $("#comentario").val();
         return true;
-    }
-
+   
 
 }
 
